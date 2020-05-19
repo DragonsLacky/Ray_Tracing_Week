a6ba4bf8f07d73bffeaf05ea0716e198d53a13e9
DragonsLacky <traykovski5@gmail.com>
2020-05-19 23:20:25 +0200

    Book 1 Chapter 4 : Completed

--- ImageOutput/ImageOutput/ImageOutput.cpp
+++ ImageOutput/ImageOutput/ImageOutput.cpp
@@ -2,8 +2,23 @@
 #include <fstream>
 #include "ray.cpp"
 
+bool hit_sphere(const vec3& center, float radius, const ray& r) 
+{
+	/*vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0f * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;*/
+	//float discriminant = b * b - 4 * a * c;
+	vec3 rc = r.point(1) - center;
+	float discriminant = dot(rc, rc) - radius * radius;
+	return discriminant <= 0;
+}
+
 vec3 color(const ray& r)
 {
+	if (hit_sphere(vec3(0, 0, -1), 0.5, r)) {
+		return vec3(1, 0, 0);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
 	float t = 0.5f * (unit_direction.y() + 1.0f);
 	return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);

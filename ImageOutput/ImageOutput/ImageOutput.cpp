fd07c01180307450f734f6ba00b102436f580b7e
DragonsLacky <traykovski5@gmail.com>
2020-05-21 16:21:03 +0200

    Book 1 - Chapter 10 : Completed

--- ImageOutput/ImageOutput/ImageOutput.cpp
+++ ImageOutput/ImageOutput/ImageOutput.cpp
@@ -38,18 +38,22 @@ int main()
 	std::ofstream fileoutput;
 	fileoutput.open("image.ppm");
 
+	float R = cos(M_PI / 4);
+
 	fileoutput << "P3\n" << nx << " " << ny << "\n255\n";
 
 	hitable* list[5];
+	//list[0] = new sphere(vec3(-R, 0, -1), R, new lambertian(vec3(0, 0, 1)));
+	//list[1] = new sphere(vec3( R, 0, -1), R, new lambertian(vec3(1, 0, 0)));
+
 	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
 	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
 	list[2] = new sphere(vec3(1.0f, 0.0f, -1.0f), 0.5, new metal(vec3(0.8, 0.6, 0.2),0.3));
 	list[3] = new sphere(vec3(-1.0, 0.0f, -1.0f), 0.5, new dielectric(1.5));
 	list[4] = new sphere(vec3(-1.0, 0.0f, -1.0f), -0.45, new dielectric(1.5));
 
-
 	hitable* world = new hitable_list(list, 5);
-	camera cam;
+	camera cam(vec3(-2, 2, 1), vec3(0, 0, -1), vec3(0, 1, 0), 20, nx / ny);
 
 	std::srand(static_cast<float>(std::time(nullptr)));
 
--- ImageOutput/ImageOutput/camera.h
+++ ImageOutput/ImageOutput/camera.h
@@ -1,16 +1,29 @@
 #pragma once
 
 #include "ray.h"
+#include <corecrt_math_defines.h>
 
 class camera
 {
 public:
-	camera()
+	camera(vec3 look_from,vec3 look_at, vec3 v_up, float v_fov, float aspect)
 	{
-		origin = vec3(0.0f, 0.0f, 0.0f);
-		lower_left_corner = vec3(-2.0f, -1.0f, -1.0f);
-		vertical = vec3(0.0f, 2.0f, 0.0f);
-		horizontal = vec3(4.0f, 0.0f, 0.0f);
+		vec3 u, v, w;
+
+		float theta = v_fov * M_PI / 180;
+		float half_height = tan(theta / 2);
+		float half_width = aspect * half_height;
+		
+		origin = look_from;
+		
+		w = unit_vector(look_from - look_at);
+		u = unit_vector(cross(v_up, w));
+		v = cross(w, u);
+
+		lower_left_corner = vec3(-half_width, -half_height, -1.0f);
+		lower_left_corner = origin - half_width * u - half_height * v - w;
+		vertical = 2 * half_height * v;
+		horizontal = 2 * half_width * u;
 	}
 	ray get_ray(float u, float v);
 	

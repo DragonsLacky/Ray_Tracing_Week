3cfedebd8f16edcd79fb2b60917a49589815082d
DragonsLacky <traykovski5@gmail.com>
2020-05-24 00:51:33 +0200

    Book 3 : Chapter 1 : PI estimate stratified

--- ImageOutput/Monte_Carlo/Monte_Carlo.cpp
+++ ImageOutput/Monte_Carlo/Monte_Carlo.cpp
@@ -6,24 +6,35 @@
 int main()
 {
     srand(static_cast<unsigned int>(time(nullptr)));
-    int runs = 0;
+    int runs = 10000;
     int inside_circle = 0;
-    while (true)
+    int inside_circle_stratified = 0;
+    for (int i = 0; i < runs; i++)
     {
-        runs++;
-        float random1 = static_cast<double>(rand()) / static_cast<double>(RAND_MAX + 1);
-        float random2 = static_cast<double>(rand()) / static_cast<double>(RAND_MAX + 1);
-        
-        float x = 2 * random1 - 1;
-        float y = 2 * random2 - 1;
-        if (x * x + y * y < 1)
+        for (int j = 0; j < runs; j++)
         {
-            inside_circle++;            
-        }
-        if (runs % 100000 == 0)
-        {
-            std::cout << "Estimate of pi = " << 4 * static_cast<float>(inside_circle) / runs++ << std::endl;
+            float random1 = static_cast<double>(rand()) / static_cast<double>(RAND_MAX + 1);
+            float random2 = static_cast<double>(rand()) / static_cast<double>(RAND_MAX + 1);
+            float x = 2 * random1 - 1;
+            float y = 2 * random2 - 1;
+            if (x * x + y * y < 1)
+            {
+                inside_circle++;            
+            }
+            random1 = static_cast<double>(rand()) / static_cast<double>(RAND_MAX + 1);
+            random2 = static_cast<double>(rand()) / static_cast<double>(RAND_MAX + 1);
+            x = 2 * ((i + random1) / runs) - 1;
+            y = 2 * ((j + random1) / runs) - 1;
+            if (x * x + y * y < 1)
+            {
+                inside_circle_stratified++;
+            }
         }
     }
+
+    std::cout << "Regular estimate of pi = " << 4 * static_cast<float>(inside_circle) / (runs*runs) << std::endl;
+    std::cout << "Stratified Estimate of pi = " << 4 * static_cast<float>(inside_circle_stratified) / (runs*runs) << std::endl;
+
+
     return 0;
 }

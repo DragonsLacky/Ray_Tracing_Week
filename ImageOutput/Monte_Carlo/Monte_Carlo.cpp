9a4d143aa8acb557cdc8659fb7caa5b49430af4d
DragonsLacky <traykovski5@gmail.com>
2020-05-24 00:43:27 +0200

    Book 3 : Chapter 1 - Made program to run infinitely

--- ImageOutput/Monte_Carlo/Monte_Carlo.cpp
+++ ImageOutput/Monte_Carlo/Monte_Carlo.cpp
@@ -6,20 +6,24 @@
 int main()
 {
     srand(static_cast<unsigned int>(time(nullptr)));
-    int N = 1000;
+    int runs = 0;
     int inside_circle = 0;
-    for (int i = 0; i < N; i++)
+    while (true)
     {
+        runs++;
         float random1 = static_cast<double>(rand()) / static_cast<double>(RAND_MAX + 1);
         float random2 = static_cast<double>(rand()) / static_cast<double>(RAND_MAX + 1);
         
-        std::cout << random1 << " " << random2 << "\n";
-
         float x = 2 * random1 - 1;
         float y = 2 * random2 - 1;
         if (x * x + y * y < 1)
+        {
             inside_circle++;            
+        }
+        if (runs % 100000 == 0)
+        {
+            std::cout << "Estimate of pi = " << 4 * static_cast<float>(inside_circle) / runs++ << std::endl;
+        }
     }
-    std::cout << "Estimate of pi = " << 4 * static_cast<float>(inside_circle) / N << std::endl;
     return 0;
 }

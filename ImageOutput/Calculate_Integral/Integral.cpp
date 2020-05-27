48199c3b5b76b55bf09923b39300e0f35889b58c
DragonsLacky <traykovski5@gmail.com>
2020-05-27 20:18:06 +0200

    Book 3 - Chapter 2 : Non-uniform pdf

--- ImageOutput/Calculate_Integral/Integral.cpp
+++ ImageOutput/Calculate_Integral/Integral.cpp
@@ -4,19 +4,19 @@
 
 inline float pdf(float x)
 {
-	return 0.5 * x;
+	return 0.5;
 }
 
 int main()
 {
 	int inside_circle = 0;
 	int inside_circle_stratified = 0;
-	int N = 10000;
+	int N = 1000000;
 	float sum = 0;
 	for (int i = 0; i < N; i++)
 	{
 		float random = static_cast<double>(rand()) / static_cast<double>(RAND_MAX + 1);
-		float x = sqrt(4 * random);
+		float x = 2 * random;
 		sum += (x * x / pdf(x));
 	}
 	std::cout << "I = " << sum / N << std::endl;

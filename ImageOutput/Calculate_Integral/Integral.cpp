3b975c1c936c366b0ed8adc84e58f50966c9313a
DragonsLacky <traykovski5@gmail.com>
2020-05-27 20:22:30 +0200

    Book 3 - Chapter 2 : MC integration

--- ImageOutput/Calculate_Integral/Integral.cpp
+++ ImageOutput/Calculate_Integral/Integral.cpp
@@ -4,19 +4,19 @@
 
 inline float pdf(float x)
 {
-	return 0.5;
+	return 3 * x * x / 8;
 }
 
 int main()
 {
 	int inside_circle = 0;
 	int inside_circle_stratified = 0;
-	int N = 1000000;
+	int N = 1;
 	float sum = 0;
 	for (int i = 0; i < N; i++)
 	{
 		float random = static_cast<double>(rand()) / static_cast<double>(RAND_MAX + 1);
-		float x = 2 * random;
+		float x = pow(8 * random, 1.0f/3.0f);
 		sum += (x * x / pdf(x));
 	}
 	std::cout << "I = " << sum / N << std::endl;

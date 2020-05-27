a4e3534783f1da2babf223ef7fd192ecbae1cfeb
DragonsLacky <traykovski5@gmail.com>
2020-05-27 20:16:28 +0200

    Book 3 - Chapter 2 : Probability distribution function

--- ImageOutput/Calculate_Integral/Integral.cpp
+++ ImageOutput/Calculate_Integral/Integral.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 
 // X^2, from 0, to 2
+
+inline float pdf(float x)
+{
+	return 0.5 * x;
+}
+
 int main()
 {
 	int inside_circle = 0;
 	int inside_circle_stratified = 0;
-	int N = 1000000;
+	int N = 10000;
 	float sum = 0;
 	for (int i = 0; i < N; i++)
 	{
 		float random = static_cast<double>(rand()) / static_cast<double>(RAND_MAX + 1);
-		float x = 2 * random;
-		sum += x * x;
+		float x = sqrt(4 * random);
+		sum += (x * x / pdf(x));
 	}
-	std::cout << "I = " << 2 * sum / N << std::endl;
+	std::cout << "I = " << sum / N << std::endl;
 }
